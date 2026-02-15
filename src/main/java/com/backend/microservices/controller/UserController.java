4acb8f5ab27c6d5459287201ddd432e678ba4a3e
shaghaf7 <shaghafkashif@gmail.com>
2026-02-15 14:26:20 +0530

    added model

---
+++ src/main/java/com/backend/microservices/controller/UserController.java
@@ -0,0 +1,4 @@
+package com.backend.microservices.controller;
+
+public class UserController {
+}
---
+++ src/main/java/com/backend/microservices/repository/UserRepository.java
@@ -0,0 +1,4 @@
+package com.backend.microservices.repository;
+
+public interface UserRepository {
+}
