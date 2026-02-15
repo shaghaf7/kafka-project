d464ecce12e270fd880f401e9e6f0dbc9581c01b
shaghaf7 <shaghafkashif@gmail.com>
2026-02-16 00:19:44 +0530

    added business logiv

--- src/main/java/com/backend/microservices/repository/BlogRepository.java
+++ src/main/java/com/backend/microservices/repository/BlogRepository.java
@@ -1,4 +1,13 @@
 package com.backend.microservices.repository;
 
-public interface BlogRepository {
+import com.backend.microservices.model.Blog;
+import com.backend.microservices.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface BlogRepository extends JpaRepository<Blog, Long> {
+
+    // Get all blogs of a specific user
+    List<Blog> findByUser(User user);
 }
