4d31c52b68a6683149192e3e7256b5efa42525d5
shaghaf7 <shaghafkashif@gmail.com>
2026-02-15 14:26:41 +0530

    added repo for it

--- src/main/java/com/backend/microservices/repository/UserRepository.java
+++ src/main/java/com/backend/microservices/repository/UserRepository.java
@@ -1,4 +1,11 @@
 package com.backend.microservices.repository;
 
-public interface UserRepository {
+import com.backend.microservices.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User,Long> {
+
+        Optional<User> findByUsername(String username);
 }
