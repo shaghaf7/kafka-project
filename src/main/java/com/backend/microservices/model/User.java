9347a7ac2c8c022b2dee7b4515adb69c2677ddca
shaghaf7 <shaghafkashif@gmail.com>
2026-02-15 16:54:22 +0530

    added jwt auth

--- src/main/java/com/backend/microservices/model/User.java
+++ src/main/java/com/backend/microservices/model/User.java
@@ -7,20 +7,22 @@ import lombok.*;
 @Table(name = "users")
 @Getter
 @Setter
-
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(unique = true,nullable = false)
+    @Column(unique = true, nullable = false)
     private String username;
 
     @Column(nullable = false)
     private String password;
 
-    @Column(unique = true,nullable = false)
+    @Column(unique = true, nullable = false)
     private String email;
 
     public String getEmail() {
--- src/main/java/com/backend/microservices/repository/UserRepository.java
+++ src/main/java/com/backend/microservices/repository/UserRepository.java
@@ -7,5 +7,7 @@ import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User,Long> {
 
-        Optional<User> findByUsername(String username);
+    void deleteByUsername(String username);
+
+    Optional<User> findByUsername(String username);
 }
